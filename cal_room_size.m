86fe36d3b24339f179a4dd224cad04e79a8aee62
Landon Wright <Landon.wright91@gmail.com>
2018-02-23 14:12:40 -0700

    added calc for number of people

--- cal_room_size.m
+++ cal_room_size.m
@@ -5,7 +5,8 @@ function [ size ] = cal_room_size( num_people, stay_length )
 % Stay_length = duration of stay in days (0 - 1825 days)
 % size = square meters of that need to be dedicated to habitations
 params;
-size = (del_size/pi)*atan((stay_length-center_offset)...
+one_room_size = (del_size/pi)*atan((stay_length-center_offset)...
     *15/tran_len) + del_size/2 + min_size;
+size = num_people * one_room_size;
 
 end
